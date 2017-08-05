182099ec70c03f6c704070e1dd8b9c779474be3c
suvedha95 <30743678+suvedha95@users.noreply.github.com>
2017-08-05 12:18:39 +0530

    Add files via upload

---
+++ count.py
@@ -0,0 +1,5 @@
+Number = int(input("Please Enter any Number: "))
+Count = 0
+while(Number > 0):
+    Number = Number // 10
+    Count = Count + 1
---
+++ hello.py
@@ -0,0 +1,4 @@
+a=1
+n=5
+for a in range(n):
+    print"hello" 
